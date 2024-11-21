d81e937bd193c590e02627a7b43021f83677f8c3
Angelus127 <valerohuertas9@gmail.com>
2024-11-20 19:35:27 -0500

    Interfaz Grafica 0.7 - interfaz de productos y navegacion

--- analytics/urls.py
+++ analytics/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('index/', views.index , name='index'),
+    path('productos/', views.product_view, name='producto')
 ]
--- analytics/views.py
+++ analytics/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'inicio/index.html')
+
+def product_view(request):
+    return render(request, 'productos/products.html')
