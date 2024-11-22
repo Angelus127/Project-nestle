e0ed67a1f89a7e3decd61ba57dde1c3d3f602b57
Angelus127 <valerohuertas9@gmail.com>
2024-11-22 10:29:38 -0500

    Modulo de productos 0.4 - estado inicial del modulo para para agregacion de productos (sin migraciones)

--- analytics/models.py
+++ analytics/models.py
@@ -4,6 +4,7 @@ class Producto(models.Model):
     nombre = models.CharField(max_length=100)
     descripcion = models.TextField()
     categoria = models.CharField(max_length=100)
+    imagen = models.ImageField(upload_to='productos/', null=True, blank=True)
 
     def __str__(self):
         return self.nombre
--- analytics/views.py
+++ analytics/views.py
@@ -1,11 +1,21 @@
-import json
 from django.shortcuts import render
+from .models import Producto
+from .forms import ProductoForm
+
 
 def index(request):
     return render(request, 'inicio/index.html')
 
 def product_view(request):
-    return render(request, 'productos/products.html')
+    if request.method == 'POST':
+        form = ProductoForm(request.Post, request.FILES)
+        if form.is_valid():
+            return redirect('Producto')
+    else:
+        form = ProductoForm()
+
+    productos = Producto.objects.all()
+    return render(request, 'productos/products.html', {'form': form, 'productos': productos})
 
 def report_view(request):
     return render(request, 'inicio/report.html')
