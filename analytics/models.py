4c703658725a96b4dac521e789ab464cf8185103
Angelus127 <valerohuertas9@gmail.com>
2024-11-21 20:02:02 -0500

    base de datos inicial 0.4 - base incial de la base de datos con una tabla basica de producto

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    categoria = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
